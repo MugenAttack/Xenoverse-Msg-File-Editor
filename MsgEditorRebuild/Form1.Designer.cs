5f2c4b09052eb6370712bbe8c77b18cafd9110b4
MugenAttack <MugenAttack@ymail.com>
2015-08-15 09:43:58 -0500

    Added Main Files

---
+++ MsgEditorRebuild/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace MsgEditorRebuild
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.slctBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LineBox = new System.Windows.Forms.ComboBox();
+            this.txtLine = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNameID = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // slctBox
+            // 
+            this.slctBox.FormattingEnabled = true;
+            this.slctBox.Location = new System.Drawing.Point(93, 46);
+            this.slctBox.Name = "slctBox";
+            this.slctBox.Size = new System.Drawing.Size(121, 21);
+            this.slctBox.TabIndex = 0;
+            this.slctBox.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select Item";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(412, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(268, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Line";
+            // 
+            // LineBox
+            // 
+            this.LineBox.FormattingEnabled = true;
+            this.LineBox.Location = new System.Drawing.Point(301, 46);
+            this.LineBox.Name = "LineBox";
+            this.LineBox.Size = new System.Drawing.Size(64, 21);
+            this.LineBox.TabIndex = 3;
+            this.LineBox.SelectedIndexChanged += new System.EventHandler(this.LineBox_SelectedIndexChanged);
+            // 
+            // txtLine
+            // 
+            this.txtLine.Location = new System.Drawing.Point(12, 73);
+            this.txtLine.Multiline = true;
+            this.txtLine.Name = "txtLine";
+            this.txtLine.Size = new System.Drawing.Size(388, 139);
+            this.txtLine.TabIndex = 5;
+            this.txtLine.TextChanged += new System.EventHandler(this.txtLine_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "NameID";
+            // 
+            // txtNameID
+            // 
+            this.txtNameID.Location = new System.Drawing.Point(84, 218);
+            this.txtNameID.Name = "txtNameID";
+            this.txtNameID.ReadOnly = true;
+            this.txtNameID.Size = new System.Drawing.Size(145, 20);
+            this.txtNameID.TabIndex = 7;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(278, 218);
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(103, 20);
+            this.txtID.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(254, 221);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "ID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 255);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNameID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtLine);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LineBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.slctBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Msg Editor Beta 4 by MugenAttack";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox slctBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox LineBox;
+        private System.Windows.Forms.TextBox txtLine;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNameID;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ MsgEditorRebuild/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Msgfile;
+namespace MsgEditorRebuild
+{
+    public partial class Form1 : Form
+    {
+        msg file;
+        string FileName;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog browseFile = new OpenFileDialog();
+            browseFile.Filter = "Xenoverse msg (*.msg)|*.msg";
+            browseFile.Title = "Browse for msg File";
+            if (browseFile.ShowDialog() == DialogResult.Cancel)
+                return;
+
+
+            FileName = browseFile.FileName;
+            file = msgStream.Load(FileName);
+
+            slctBox.Items.Clear();
+            for (int i = 0; i < file.data.Length; i++)
+                slctBox.Items.Add(i);
+
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            msgStream.Save(file, FileName);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtNameID.Text = file.data[slctBox.SelectedIndex].NameID;
+            txtID.Text = file.data[slctBox.SelectedIndex].ID.ToString();
+            LineBox.Items.Clear();
+            for (int i = 0; i < file.data[slctBox.SelectedIndex].Lines.Length; i++)
+                LineBox.Items.Add(i);
+
+            LineBox.SelectedIndex = 0;
+            txtLine.Text = file.data[slctBox.SelectedIndex].Lines[LineBox.SelectedIndex];
+        }
+
+        private void LineBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtLine.Text = file.data[slctBox.SelectedIndex].Lines[LineBox.SelectedIndex];
+        }
+
+        private void txtLine_TextChanged(object sender, EventArgs e)
+        {
+            file.data[slctBox.SelectedIndex].Lines[LineBox.SelectedIndex] = txtLine.Text;
+        }
+    }
+}
